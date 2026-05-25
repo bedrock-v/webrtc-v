afcc98296fd0b87158929cc52d1a90324f2f8972
xRookieFight <me@xrookiefight.com>
2026-05-25 11:50:45 +0300

    feat(codec): add Writer.u8

--- internal/codec/writer.v
+++ internal/codec/writer.v
@@ -31,3 +31,12 @@ pub fn Writer.with_capacity(n int) Writer {
 pub fn (w &Writer) len() int {
 	return w.buf.len
 }
+
+pub fn (mut w Writer) u8(v u8) {
+	w.buf << v
+}
+
+pub fn (mut w Writer) u16(v u16) {
+	w.buf << u8(v >> 8)
+	w.buf << u8(v)
+}
