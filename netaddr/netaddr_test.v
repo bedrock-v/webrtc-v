0d8621dcbac715fabae8abc72fa009f0b9b957d1
xRookieFight <me@xrookiefight.com>
2026-05-28 20:45:12 +0300

    test(netaddr): ipv4 parse and format

--- netaddr/netaddr_test.v
+++ netaddr/netaddr_test.v
@@ -1,1 +1,11 @@
 module netaddr
+
+fn test_ipv4_parse_and_format() {
+	cases := ['0.0.0.0', '127.0.0.1', '192.168.1.1', '255.255.255.255', '8.8.8.8']
+	for c in cases {
+		addr := IpAddr.parse(c)!
+		assert addr.family == .ipv4
+		assert addr.octets.len == 4
+		assert addr.str() == c
+	}
+}
