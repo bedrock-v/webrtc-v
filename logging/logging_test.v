e71c2e068d4ddfcf63d1cf169ed72416bb6e6762
xRookieFight <me@xrookiefight.com>
2026-05-29 16:38:26 +0300

    feat(logging): add CaptureSink.write

--- logging/logging_test.v
+++ logging/logging_test.v
@@ -7,3 +7,10 @@ mut:
 	mu      &sync.Mutex = sync.new_mutex()
 	records []string
 }
+
+fn (s &CaptureSink) write(level Level, scope string, msg string) {
+	mut m := unsafe { s }
+	m.mu.lock()
+	m.records << '${level}|${scope}|${msg}'
+	m.mu.unlock()
+}
