c8e9f0703e4142b43e6e2f0c3699b8752f900af0
xRookieFight <me@xrookiefight.com>
2026-05-31 10:53:50 +0300

    feat(stun): add the attr_username constant

--- stun/attributes.v
+++ stun/attributes.v
@@ -8,3 +8,11 @@ module stun
 // range makes the whole message unprocessable and must be answered with a 420
 // error listing it.
 pub const attr_mapped_address = u16(0x0001)
+pub const attr_username = u16(0x0006)
+pub const attr_message_integrity = u16(0x0008)
+pub const attr_error_code = u16(0x0009)
+pub const attr_unknown_attributes = u16(0x000A)
+pub const attr_channel_number = u16(0x000C)
+pub const attr_lifetime = u16(0x000D)
+pub const attr_xor_peer_address = u16(0x0012)
+pub const attr_data = u16(0x0013)
