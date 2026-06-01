a48e35d40c459784b641fef446ff925a9c7989b1
xRookieFight <me@xrookiefight.com>
2026-06-01 17:13:23 +0300

    feat(stun): add the magic_cookie constant

--- stun/message.v
+++ stun/message.v
@@ -6,3 +6,8 @@ import crypto.sha256
 import hash.crc32
 import webrtc.internal.codec
 import webrtc.internal.randutil
+
+// magic_cookie is the fixed value in bytes 4..8 of every STUN message
+// (RFC 8489 section 5). It is what lets a receiver tell STUN apart from other
+// protocols multiplexed on the same socket.
+pub const magic_cookie = u32(0x2112A442)
