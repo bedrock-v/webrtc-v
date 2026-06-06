8473e6b32e2b64c58fa105a131189cd0b8e810a1
xRookieFight <me@xrookiefight.com>
2026-06-06 16:39:33 +0300

    test(stun): error code round trip

--- stun/attrs_test.v
+++ stun/attrs_test.v
@@ -1,1 +1,13 @@
 module stun
+
+fn test_error_code_round_trip() {
+	codes := [300, 400, 401, 420, 438, 487, 500, 699]
+	for code in codes {
+		mut msg := Message.new(.error_response, .binding)!
+		msg.add_error_code(code, '')!
+		decoded := Message.decode(msg.encode()!)!
+		got := decoded.error_code()!
+		assert got.code == code
+		assert got.reason == default_reason(code)
+	}
+}
