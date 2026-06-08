719d11106b2eedca2dcd1c69f937c870369d99ce
xRookieFight <me@xrookiefight.com>
2026-06-09 00:01:13 +0300

    feat(stun): add the vector_response_v4 constant

--- stun/rfc5769_test.v
+++ stun/rfc5769_test.v
@@ -18,3 +18,8 @@ const vector_request = '000100582112a442b7e7a701bc34d686fa87dfae' + '80220010' +
 const vector_request_username = 'evtj:h6vY'
 const vector_request_password = 'VOkJxbRl1RmTxUk/WvJxBt'
 const vector_request_software = 'STUN test client'
+
+// Section 2.2: sample IPv4 success response.
+const vector_response_v4 = '0101003c2112a442b7e7a701bc34d686fa87dfae' + '8022000b' +
+	'74657374207665' + '63746f7220' + '00200008' + '0001a147e112a643' + '00080014' +
+	'2b91f599fd9e90c38c7489f92af9ba53f06be7d7' + '80280004' + 'c07d4c96'
