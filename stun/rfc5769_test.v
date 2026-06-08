d5d6842bb9d1f3905423dc364ef2b1791678041a
xRookieFight <me@xrookiefight.com>
2026-06-09 00:00:36 +0300

    feat(stun): extend rfc5769_test.v

--- stun/rfc5769_test.v
+++ stun/rfc5769_test.v
@@ -11,3 +11,10 @@ import webrtc.netaddr
 
 // Section 2.1: sample request from an ICE client.
 const vector_request = '000100582112a442b7e7a701bc34d686fa87dfae' + '80220010' +
+	'5354554e2074657374' + '20636c69656e74' + '00240004' + '6e0001ff' + '80290008' +
+	'932ff9b151263b36' + '00060009' + '6576746a3a68367659202020' + '00080014' +
+	'9aeaa70cbfd8cb56781ef2b5b2d3f249c1b571a2' + '80280004' + 'e57a3bcf'
+
+const vector_request_username = 'evtj:h6vY'
+const vector_request_password = 'VOkJxbRl1RmTxUk/WvJxBt'
+const vector_request_software = 'STUN test client'
