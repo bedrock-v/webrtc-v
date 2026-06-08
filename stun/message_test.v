f99015bb220fb75d33ec0edba331047d94c009cd
xRookieFight <me@xrookiefight.com>
2026-06-08 20:37:11 +0300

    test(stun): unknown comprehension required detection

--- stun/message_test.v
+++ stun/message_test.v
@@ -328,3 +328,16 @@ fn test_missing_fingerprint_is_reported() {
 	}
 	assert false, 'missing FINGERPRINT must be reported'
 }
+
+fn test_unknown_comprehension_required_detection() {
+	mut msg := Message.new(.request, .binding)!
+	msg.add_username('u')!
+	msg.add(0x7FFF, [u8(1), 2, 3, 4]) // comprehension-required, unknown
+	msg.add(0x8FFF, [u8(1), 2, 3, 4]) // comprehension-optional, unknown
+	msg.add(0x7FFF, [u8(5), 6, 7, 8]) // duplicate: reported once
+
+	unknown := msg.unknown_comprehension_required([attr_username])
+	assert unknown == [u16(0x7FFF)]
+	assert is_comprehension_required(0x7FFF)
+	assert !is_comprehension_required(0x8000)
+}
