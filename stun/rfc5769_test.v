cfdb78a0888fccedc2c1dc6cf007eb20be6d2907
xRookieFight <me@xrookiefight.com>
2026-06-09 15:19:52 +0300

    test(stun): rfc5769 ipv4 response reencodes

--- stun/rfc5769_test.v
+++ stun/rfc5769_test.v
@@ -164,3 +164,18 @@ fn test_rfc5769_ipv6_response() {
 	msg.check_message_integrity(short_term_key(vector_response_password)!)!
 	msg.check_fingerprint()!
 }
+
+fn test_rfc5769_ipv4_response_reencodes() {
+	raw := hex.decode(vector_response_v4)!
+	decoded := Message.decode(raw)!
+
+	mut rebuilt := Message.with_transaction_id(.success_response, .binding, decoded.transaction_id)
+	rebuilt.add_software(vector_response_software)!
+	rebuilt.add_xor_mapped_address(netaddr.SocketAddr.parse('192.0.2.1:32853')!)!
+
+	out := rebuilt.encode(
+		integrity_key: short_term_key(vector_response_password)!
+		fingerprint:   true
+	)!
+	assert_matches_reference(out, raw, vector_response_password)!
+}
