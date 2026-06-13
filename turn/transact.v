97cd8098e8a7b35288a87521044c497c5e4018dd
xRookieFight <me@xrookiefight.com>
2026-06-13 11:16:39 +0300

    feat(turn): add Client.local_address

--- turn/transact.v
+++ turn/transact.v
@@ -384,3 +384,13 @@ fn (mut c Client) maintain() {
 pub fn (c &Client) server_address() netaddr.SocketAddr {
 	return c.server
 }
+
+// local_address is the address of the socket facing the relay.
+pub fn (c &Client) local_address() !netaddr.SocketAddr {
+	return transport.local_addr(c.conn) or {
+		return TurnError{
+			reason: .transport
+			detail: err.msg()
+		}
+	}
+}
