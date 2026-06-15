c7e0b89c82cab14134e2f12523782ec7bf073c14
xRookieFight <me@xrookiefight.com>
2026-06-15 15:04:59 +0300

    test(sdp): marshal emits default time line

--- sdp/sdp_test.v
+++ sdp/sdp_test.v
@@ -399,3 +399,26 @@ fn test_repeat_times_with_unit_suffixes() {
 	assert repeat.offsets == [u64(0), 25 * 3600]
 	assert s.marshal().contains('r=604800 3600 0 90000')
 }
+
+fn test_marshal_emits_default_time_line() {
+	s := SessionDescription{
+		origin: Origin{
+			session_id:      1
+			session_version: 1
+		}
+	}
+	out := s.marshal()
+	assert out.contains('t=0 0\r\n')
+	assert out.starts_with('v=0\r\n')
+	assert out.contains('s=-\r\n')
+}
+
+fn test_connection_with_ttl_round_trips() {
+	doc := 'v=0\r\no=- 1 1 IN IP4 127.0.0.1\r\ns=-\r\nc=IN IP4 224.2.1.1/127/3\r\nt=0 0\r\n'
+	s := parse(doc)!
+	conn := s.connection?
+	assert conn.address == '224.2.1.1'
+	assert conn.ttl == 127
+	assert conn.range == 3
+	assert s.marshal() == doc
+}
