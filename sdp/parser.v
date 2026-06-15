0431e58060271e8d0038d10e20d22e40b519ec56
xRookieFight <me@xrookiefight.com>
2026-06-16 00:09:51 +0300

    feat(sdp): add the ParseError struct

--- sdp/parser.v
+++ sdp/parser.v
@@ -1,1 +1,10 @@
 module sdp
+
+// ParseError describes where and why a description could not be parsed. The
+// line number lets an operator find the offending line in a description that is
+// often hundreds of lines long.
+pub struct ParseError {
+pub:
+	line   int
+	detail string
+}
