3582e86cbf2f074bf901f5f577687903dc7ab9a1
xRookieFight <me@xrookiefight.com>
2026-06-17 10:56:42 +0300

    feat(rtp): add Sequencer.new

--- rtp/sequence.v
+++ rtp/sequence.v
@@ -19,3 +19,22 @@ mut:
 	roll_over_count u32
 	started         bool
 }
+
+// Sequencer.new returns a sequencer starting from a random point.
+pub fn Sequencer.new() !Sequencer {
+	start := randutil.next_u16()!
+	return Sequencer{
+		sequence_number: start
+		started:         false
+	}
+}
+
+// Sequencer.starting_at returns a sequencer with a chosen initial value. It
+// exists for tests and for resuming a stream; new streams should use
+// Sequencer.new.
+pub fn Sequencer.starting_at(sequence_number u16) Sequencer {
+	return Sequencer{
+		sequence_number: sequence_number
+		started:         false
+	}
+}
