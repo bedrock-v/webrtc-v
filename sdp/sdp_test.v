7a7621778c40c7c2838c3b3e21e98401cf36da14
xRookieFight <me@xrookiefight.com>
2026-06-18 21:40:07 +0300

    test(sdp): round trip preserves unknown attributes

--- sdp/sdp_test.v
+++ sdp/sdp_test.v
@@ -323,3 +323,10 @@ fn test_round_trip_is_stable() {
 	assert once.contains('a=rtcp:9 IN IP4 0.0.0.0')
 	assert once.contains('a=end-of-candidates')
 }
+
+fn test_round_trip_preserves_unknown_attributes() {
+	doc := 'v=0\r\no=- 1 1 IN IP4 127.0.0.1\r\ns=-\r\nt=0 0\r\n' +
+		'a=x-vendor-session:something\r\n' + 'm=audio 9 UDP/TLS/RTP/SAVPF 111\r\n' +
+		'a=x-vendor-media:other\r\n' + 'a=x-flag\r\n'
+	assert parse(doc)!.marshal() == doc
+}
