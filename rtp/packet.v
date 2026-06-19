124ea1c6f0a49b52ec4aee918acc92ec22d2811b
xRookieFight <me@xrookiefight.com>
2026-06-19 17:45:05 +0300

    feat(rtp): add Header.extension

--- rtp/packet.v
+++ rtp/packet.v
@@ -106,3 +106,13 @@ pub fn (h &Header) has_extensions() bool {
 pub fn (h &Header) uses_two_byte_extensions() bool {
 	return h.extension_profile & 0xFFF0 == extension_profile_two_byte_base
 }
+
+// extension returns the payload of the extension with the given id.
+pub fn (h &Header) extension(id u8) ?[]u8 {
+	for ext in h.extensions {
+		if ext.id == id {
+			return ext.payload
+		}
+	}
+	return none
+}
