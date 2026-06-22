b4d44bc00450dd2f833c55ad68b6755ba6bf718f
xRookieFight <me@xrookiefight.com>
2026-06-22 13:52:05 +0300

    feat(rtcp): add the sdes_location constant

--- rtcp/sdes.v
+++ rtcp/sdes.v
@@ -8,3 +8,10 @@ pub const sdes_cname = u8(1)
 pub const sdes_name = u8(2)
 pub const sdes_email = u8(3)
 pub const sdes_phone = u8(4)
+pub const sdes_location = u8(5)
+pub const sdes_tool = u8(6)
+pub const sdes_note = u8(7)
+pub const sdes_private = u8(8)
+
+// max_sdes_item_bytes is the largest item the 8-bit length field can express.
+pub const max_sdes_item_bytes = 255
