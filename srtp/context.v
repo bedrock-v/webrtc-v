9028317b4ee64470184c46d66984946c576106ad
xRookieFight <me@xrookiefight.com>
2026-06-26 23:58:15 +0300

    feat(srtp): add the max_srtcp_index constant

--- srtp/context.v
+++ srtp/context.v
@@ -11,3 +11,8 @@ const srtcp_header_size = 8
 
 // srtcp_index_size is the size of the trailing E-flag and index field.
 const srtcp_index_size = 4
+
+// max_srtcp_index is the largest value the 31-bit SRTCP index can hold. Once it
+// is reached the master key must be replaced; continuing would repeat a counter
+// block and destroy confidentiality.
+const max_srtcp_index = u32(0x7FFFFFFF)
