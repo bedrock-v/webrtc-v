e88be6c45226860d2f87f3e21b01b734239330a8
xRookieFight <me@xrookiefight.com>
2026-07-15 13:10:00 +0300

    feat(dtls): add unmarshal_finished

--- dtls/handshake.v
+++ dtls/handshake.v
@@ -884,3 +884,20 @@ fn unmarshal_client_key_exchange(body []u8) !ClientKeyExchange {
 		public_key: public_key
 	}
 }
+
+fn unmarshal_finished(body []u8) !Finished {
+	if body.len != verify_data_length {
+		return HandshakeError{
+			detail: 'Finished is ${body.len} bytes, expected ${verify_data_length}'
+		}
+	}
+	return Finished{
+		verify_data: body.clone()
+	}
+}
+
+fn short(name string) HandshakeError {
+	return HandshakeError{
+		detail: '${name} is shorter than its fields require'
+	}
+}
