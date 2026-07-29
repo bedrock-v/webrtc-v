c7a1ce7d05b3208555c406d634d0be67b0064892
xRookieFight <me@xrookiefight.com>
2026-07-29 14:39:57 +0300

    feat(sctp): add crc32c

--- sctp/crc32c.v
+++ sctp/crc32c.v
@@ -38,3 +38,12 @@ fn build_crc32c_table() []u32 {
 	}
 	return table
 }
+
+// crc32c computes the CRC-32c of a byte string.
+pub fn crc32c(data []u8) u32 {
+	mut crc := u32(0xFFFFFFFF)
+	for b in data {
+		crc = (crc >> 8) ^ crc32c_table[(crc ^ u32(b)) & 0xFF]
+	}
+	return crc ^ 0xFFFFFFFF
+}
