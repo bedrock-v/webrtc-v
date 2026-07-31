7f66c4ee61725c1373997e6fc81539d4412c04e7
xRookieFight <me@xrookiefight.com>
2026-07-31 15:56:57 +0300

    feat(sctp): add Data.flags

--- sctp/chunks_typed.v
+++ sctp/chunks_typed.v
@@ -220,3 +220,20 @@ pub mut:
 	// latency for a small message at the cost of an extra packet.
 	immediate_sack bool
 }
+
+fn (d Data) flags() u8 {
+	mut flags := u8(0)
+	if d.end {
+		flags |= data_flag_end
+	}
+	if d.beginning {
+		flags |= data_flag_beginning
+	}
+	if d.unordered {
+		flags |= data_flag_unordered
+	}
+	if d.immediate_sack {
+		flags |= data_flag_immediate_sack
+	}
+	return flags
+}
