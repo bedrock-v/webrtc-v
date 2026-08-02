7fa5418ac87247535d77dd779a24d10fb2f944f0
xRookieFight <me@xrookiefight.com>
2026-08-02 12:07:31 +0300

    feat(sctp): add unmarshal_error_causes

--- sctp/chunks_typed.v
+++ sctp/chunks_typed.v
@@ -464,3 +464,15 @@ fn marshal_error_causes(causes []ErrorCause) ![]u8 {
 	}
 	return marshal_parameters(parameters)!
 }
+
+fn unmarshal_error_causes(value []u8) ![]ErrorCause {
+	parameters := unmarshal_parameters(value)!
+	mut out := []ErrorCause{cap: parameters.len}
+	for parameter in parameters {
+		out << ErrorCause{
+			code:  parameter.typ
+			value: parameter.value
+		}
+	}
+	return out
+}
