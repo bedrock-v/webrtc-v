1b034a0af7859c8cb1c6335d92810ba2cfeb5a26
xRookieFight <me@xrookiefight.com>
2026-08-13 16:40:57 +0300

    feat(webrtc): scaffold peer_connection.v

---
+++ peer_connection.v
@@ -0,0 +1,11 @@
+module webrtc
+
+import sync
+import time
+import webrtc.datachannel
+import webrtc.dtls
+import webrtc.ice
+import webrtc.logging
+import webrtc.sctp
+import webrtc.sdp
+import webrtc.srtp
