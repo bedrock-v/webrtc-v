c10516a20ae1ffca6672bc3d0984d0965a04317f
xRookieFight <me@xrookiefight.com>
2026-08-16 00:04:18 +0300

    feat(webrtc): add DataChannel.try_recv

--- data_channel.v
+++ data_channel.v
@@ -169,3 +169,16 @@ pub fn (mut d DataChannel) recv(timeout time.Duration) !DataChannelMessage {
 		data:      message.data
 	}
 }
+
+// try_recv returns a message if one is already queued.
+pub fn (mut d DataChannel) try_recv() ?DataChannelMessage {
+	mut channel := d.channel
+	if d.closed || channel == unsafe { nil } {
+		return none
+	}
+	message := channel.try_recv()?
+	return DataChannelMessage{
+		is_string: message.is_string
+		data:      message.data
+	}
+}
