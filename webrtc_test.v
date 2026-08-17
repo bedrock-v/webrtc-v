0bc5bc14849d13652b75d74f2c03b329aa0ba4c4
xRookieFight <me@xrookiefight.com>
2026-08-17 17:49:25 +0300

    test(webrtc): a channel created before negotiation is connecting

--- webrtc_test.v
+++ webrtc_test.v
@@ -293,3 +293,22 @@ fn test_media_calls_fail_without_a_media_section() {
 		}
 	}
 }
+
+fn test_a_channel_created_before_negotiation_is_connecting() {
+	mut pc := PeerConnection.new()!
+	defer {
+		pc.close()
+	}
+	mut channel := pc.create_data_channel('chat')!
+	assert channel.label == 'chat'
+	assert channel.state() == .connecting
+	assert channel.ordered()
+	assert channel.reliable()
+	assert channel.id() == none
+
+	if _ := channel.send_text('too early') {
+		assert false, 'a channel that is not open should refuse to send'
+	} else {
+		assert err is PeerError
+	}
+}
