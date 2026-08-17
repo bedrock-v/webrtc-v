8e7c309d56385ffe108a568061675c55166f3eb8
xRookieFight <me@xrookiefight.com>
2026-08-17 21:00:15 +0300

    test(webrtc): an answer cannot be applied without an offer

--- webrtc_test.v
+++ webrtc_test.v
@@ -227,3 +227,35 @@ fn test_an_answer_may_not_leave_the_roles_undetermined() {
 		}
 	}
 }
+
+fn test_an_answer_cannot_be_applied_without_an_offer() {
+	mut pc := PeerConnection.new()!
+	defer {
+		pc.close()
+	}
+	pc.create_data_channel('chat')!
+	offer := pc.create_offer()!
+	if _ := pc.set_remote_description(SessionDescription{ typ: .answer, sdp: offer.sdp }) {
+		assert false, 'an answer needs a local offer first'
+	} else {
+		assert err is PeerError
+		if err is PeerError {
+			assert err.reason == .wrong_state
+		}
+	}
+}
+
+fn test_a_candidate_cannot_be_added_before_a_local_description() {
+	mut pc := PeerConnection.new()!
+	defer {
+		pc.close()
+	}
+	if _ := pc.add_ice_candidate('candidate:1 1 udp 2130706431 127.0.0.1 4000 typ host') {
+		assert false, 'there is nothing to add a candidate to yet'
+	} else {
+		assert err is PeerError
+		if err is PeerError {
+			assert err.reason == .wrong_state
+		}
+	}
+}
