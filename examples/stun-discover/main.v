350babb9783233a95b6176bbbee6d67ab44ddeb0
xRookieFight <me@xrookiefight.com>
2026-08-18 22:31:35 +0300

    feat(examples): add the stun-discover example

--- examples/stun-discover/main.v
+++ examples/stun-discover/main.v
@@ -15,3 +15,31 @@ import webrtc.logging
 import webrtc.stunclient
 
 const default_server = 'stun.l.google.com:19302'
+
+fn main() {
+	server := if os.args.len > 1 { os.args[1] } else { default_server }
+
+	println('asking ${server}...')
+	started := time.now()
+
+	address := stunclient.discover(server,
+		rto:               500 * time.millisecond
+		max_transmissions: 5
+		logger:            logging.from_env('stun')
+	) or {
+		eprintln('failed: ${err}')
+		exit(1)
+	}
+
+	elapsed := time.now() - started
+	println('the internet sees this machine as ${address}')
+	println('  family:  ${address.ip.family}')
+	println('  private: ${address.ip.is_private()}')
+	println('  took:    ${elapsed.milliseconds()}ms')
+
+	if address.ip.is_private() {
+		println('')
+		println('The reflexive address is private, so this machine is either not')
+		println('behind a NAT or is behind one that does not translate.')
+	}
+}
